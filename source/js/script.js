fd546b14c7d93cbe14b627faf74df8f871521b9b
Ilya Koren <ilyakoren@mail.ru>
2021-05-20 10:11:01 +0300

    Вносит правки

--- source/js/script.js
+++ source/js/script.js
@@ -35,3 +35,10 @@ for (let anchor of anchors) {
     });
   });
 }
+
+
+const element = document.querySelector('.input-email');
+const maskOptions = {
+  mask: '+{7}(000)000-00-00'
+};
+const mask = IMask(element, maskOptions);
